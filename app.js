40d907d43fcbb907f9dc63d9fe822d9586d558c6
Connor Grieves <connor@Connors-MBP.lan>
2019-03-27 15:03:25 +0000

    Fixed issue where dice image would not reset upon pressing New Game.

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ function init () {
   scores = [0, 0];
   roundScore = 0;
   activePlayer = 0;
+  diceDOM.style.display = 'none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
   document.getElementById('current-0').textContent = '0';
